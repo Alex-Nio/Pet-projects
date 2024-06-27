a14397dea31360fd6b6e7cc8c36b7672fb1bc81e
Alex Nio <volpenio@gmail.com>
2024-06-28 04:32:55 +0500

    add project

---
+++ video-hosting-vue2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="full-wrapper">
+    <header>
+      <nav class="nav">
+        <router-link class="nav-item" to="/">На Главную</router-link>
+      </nav>
+    </header>
+    <main class="main">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./assets/scss/imports.scss";
+header {
+  height: 80px;
+}
+
+.main {
+  margin: 45px 0 420px 0;
+}
+
+.nav {
+  @include fdrjs_aic;
+  width: 100%;
+  padding: 20px 0;
+  height: 80px;
+}
+.nav-item {
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 10px 20px;
+}
+</style>
