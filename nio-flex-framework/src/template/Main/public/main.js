eeba961d1fd1d55556500b1287872e0c987ad695
Alex Nio <volpenio@gmail.com>
2024-06-28 07:19:03 +0500

    add fremework

---
+++ nio-flex-framework/src/template/Main/public/main.js
@@ -0,0 +1,8 @@
+import ResizeObserver from 'resize-observer-polyfill';
+window.ResizeObserver = ResizeObserver;
+//* Libs
+import './../../../app/js/utils/handlers.js';
+//? Common Components
+import './../../../components/Common/footer/main-footer/footer.js';
+//? Shared Components
+//? Chapter Unique Components
